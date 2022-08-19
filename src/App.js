20a913ee710ac43a07d494497a99bb1ab49a62c2
Haseesah <6310110606@psu.ac.th>
2022-08-19 15:37:43 +0700

    part 7_1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 function App() {
   return (
     <div>
+      <h1>GAME</h1>
+      <p>เกมเรียงตัวอักษร</p>
       <WordCard value={word}/>
     </div>
   );
 }
+
 
 export default App;
